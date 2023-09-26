96d7dd44d9f9788f80066b7486e65a8abe3230a7
Rabin Shrestha <sanayashe@gmail.com>
2023-09-26 16:20:34 +0545

    multiple fetch returns data

--- src/tutorial/03-conditional-rendering/starter/01-multiple-returns-basics.jsx
+++ src/tutorial/03-conditional-rendering/starter/01-multiple-returns-basics.jsx
@@ -1,6 +1,19 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 
 const MultipleReturnsBasics = () => {
-  return <h2>Multiple Returns Basics</h2>;
+	const [isLoading, setIsLoading] = useState(true);
+
+	useEffect(() => {
+		setTimeout(() => {
+			//dont fetching data
+			setIsLoading(false);
+		}, 3000);
+	}, []);
+
+	if (isLoading) {
+		return <h1>Loading...</h1>;
+	}
+
+	return <h2>Multiple Returns Basics</h2>;
 };
 export default MultipleReturnsBasics;
--- src/tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data.jsx
+++ src/tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data.jsx
@@ -1,7 +1,44 @@
-import { useEffect, useState } from 'react';
-const url = 'https://api.github.com/users/QuincyLarson';
+import { useEffect, useState } from "react";
+const url = "https://api.github.com/users/QuincyLarson";
 
 const MultipleReturnsFetchData = () => {
-  return <h2>Fetch Data </h2>;
+	const [isLoading, setIsLoading] = useState(true);
+	const [isError, setIsError] = useState(false);
+	const [user, setUser] = useState(null);
+
+	useEffect(() => {
+		const fetchUser = async () => {
+			try {
+				const response = await fetch(url);
+				const user = await response.json();
+				setUser(user);
+			} catch (error) {
+				console.log(error);
+			}
+			setIsLoading(false);
+		};
+		fetchUser();
+	}, []);
+
+	if (isLoading) {
+		return <h2>Loading...</h2>;
+	}
+
+	if (isError) {
+		return <h2>There was an error...</h2>;
+	}
+
+	return (
+		<>
+			<img
+				style={{ width: "150px", borderRadius: "25px" }}
+				src={user.avatar_url}
+				alt={user.name}
+			/>
+			<h2>{user.name}</h2>
+			<h4>works at {user.company}</h4>
+			<p>{user.bio}</p>
+		</>
+	);
 };
 export default MultipleReturnsFetchData;
