700eb2911c8b4e22e426f01ac6365035b02371bd
Rabin Shrestha <sanayashe@gmail.com>
2023-10-08 22:07:15 +0545

    useReducer

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import Starter from "./tutorial/09-context-api/starter/Navbar";
-import Final from "./tutorial/09-context-api/final/context/Navbar";
+import Starter from "./tutorial/10-useReducer/starter/01-useReducer";
+
 function App() {
 	return (
 		<>
-			<Final />
+			{/* <Final /> */}
 			<Starter />
 		</>
 	);
